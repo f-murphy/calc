88e4d5c4207086c2375d0a8cc720614a427da202
f-murphy <100728686+f-murphy@users.noreply.github.com>
2022-10-25 23:41:25 +0300

    addCalculation

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,4 +1,28 @@
-﻿class Program
-{
+﻿using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Введите выражение: ");
+                Console.WriteLine(Calculation.Calculate(Console.ReadLine()));
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Проверьте правильность вводимых данных.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Используйте вместо точки - запятую");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Делить на ноль нельзя");
+            }
+        }
+    }
 }
