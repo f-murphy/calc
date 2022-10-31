32d2f528653d8dcb19d4ce19f3d84a548194f018
f-murphy <murphy.fariz@gmail.com>
2022-10-31 18:25:40 +0300

    replace

---
+++ TestProject1/CalculationTests.cs
@@ -0,0 +1,17 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class CalculationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            
+            // Act
+
+            // Assert
+
+        }
+    }
+}
